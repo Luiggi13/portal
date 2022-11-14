d54ea3b83ce3acacea8126afa2f48c871ba0d5f0
Christian Llansola <christian.llansola@mail.schwarz>
2022-11-14 17:39:11 +0100

    Routes just for Christmas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'auth',
-    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
-  },
-  { path: 'dash',
-    loadChildren: () => import('./modules/dashboard/dash.module').then(m => m.DashModule)
-  },
   { path: 'christmas',
     loadChildren: () => import('./modules/christmas/christmas.module').then(m => m.ChristmasModule)
-  },
-  { path: 'christmas-new',
-  loadChildren: () => import('./modules/christmas2/christmas-new.module').then(m => m.ChristmasNewModule)
   },
   { path: '**',
     redirectTo: 'christmas'
