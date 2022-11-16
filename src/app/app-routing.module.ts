c0c87852082ec4bd53547f87809d73e695bc20ff
Christian Llansola <christian.llansola@mail.schwarz>
2022-11-16 12:32:46 +0100

    For Xifre

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: 'dash',
+    loadChildren: () => import('./modules/dashboard/dash.module').then(m => m.DashModule)
+  },
   { path: 'christmas',
     loadChildren: () => import('./modules/christmas/christmas.module').then(m => m.ChristmasModule)
   },
-  { path: '**',
-    redirectTo: 'christmas'
-  },
+  // { path: '**',
+  //   redirectTo: 'christmas'
+  // },
 ];
 
 @NgModule({
